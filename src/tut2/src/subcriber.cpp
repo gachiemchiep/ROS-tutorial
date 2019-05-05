3e712cb37892c806454d83c5ba77609b629c8f70
vu gia truong <gachiemchiep@gmail.com>
2019-05-05 14:22:10 +0900

    Add tut2

---
+++ src/tut2/src/subcriber.cpp
@@ -0,0 +1,27 @@
+/*
+ * subcriber.cpp
+ *
+ *  Created on: May 5, 2019
+ *      Author: gachiemchiep
+ */
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+void chatterCallbackk(const std_msgs::String::ConstPtr& msg) {
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "subcriber");
+
+	ros::NodeHandle nh;
+
+	ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallbackk);
+
+	ros::spin();
+
+	return 0;
+}
+
+
